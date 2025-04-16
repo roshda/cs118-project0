1a3488d6aa3329f0ea99e147cb8483e270c3d7b7
Roshni Dave <79119564+roshda@users.noreply.github.com>
2025-04-16 13:56:18 -0700

    done

--- project/client.c
+++ project/client.c
@@ -1,37 +1,97 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <netdb.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+// helper to set a file descriptor to non-blocking mode
+static int set_nb(int fd) {
+    int f = fcntl(fd, F_GETFL, 0);
+    return f < 0 ? -1 : fcntl(fd, F_SETFL, f | O_NONBLOCK);
+}
+
 int main(int argc, char** argv) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: client <hostname> <port> \n");
-        exit(1);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: client <hostname> <port>\n");
+        return 1;
     }
 
-    // Only supports localhost as a hostname, but that's all we'll test on
-    const char* addr =
-        strcmp(argv[1], "localhost") == 0 ? "127.0.0.1" : argv[1];
+    // TODO: resolve hostname to IP
+    // if it's literally "localhost", just use 127.0.0.1
+    const char* host = strcmp(argv[1], "localhost") == 0 ? "127.0.0.1" : argv[1];
     int port = atoi(argv[2]);
 
-    // TODO: Create socket
-
-    // TODO: Set stdin and socket nonblocking
-
-    // TODO: Construct server address
-    
-    char buffer[1024];
-
-    // Listen loop
-    while (1) {
-        // TODO: Receive from socket
-        // TODO: If data, write to stdout
-        // TODO: Read from stdin
-        // TODO: If data, send to socket
+    // TODO: create UDP socket
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
     }
 
+    // TODO: set stdin and socket non-blocking
+    if (set_nb(STDIN_FILENO) < 0 || set_nb(sock) < 0) {
+        perror("fcntl");
+        return 1;
+    }
+
+    // TODO: construct server sockaddr_in
+    struct sockaddr_in srv = {0};
+    srv.sin_family = AF_INET;
+    srv.sin_port = htons(port);
+    if (inet_pton(AF_INET, host, &srv.sin_addr) != 1) {
+        fprintf(stderr, "bad address\n");
+        return 1;
+    }
+
+    // poll both stdin and socket
+    struct pollfd pfds[2] = {
+        { .fd = STDIN_FILENO, .events = POLLIN },
+        { .fd = sock,         .events = POLLIN }
+    };
+
+    char buf[1024];  // buffer size must be 1024
+    int stdin_closed = 0;
+
+    // main loop: send stdin to server, print from server to stdout
+    while (1) {
+        if (poll(pfds, 2, -1) < 0) {
+            perror("poll");
+            break;
+        }
+
+        // TODO: receive from server
+        if (pfds[1].revents & POLLIN) {
+            ssize_t n = recvfrom(sock, buf, sizeof(buf), 0, NULL, NULL);
+            if (n > 0) {
+                // TODO: if data, write to stdout
+                write(STDOUT_FILENO, buf, n);
+            } else if (n < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+                perror("recvfrom");
+            }
+        }
+
+        // TODO: read from stdin
+        // TODO: if data, send to server
+        if (!stdin_closed && (pfds[0].revents & POLLIN)) {
+            ssize_t m = read(STDIN_FILENO, buf, sizeof(buf));
+            if (m > 0) {
+                sendto(sock, buf, m, 0,
+                       (struct sockaddr*)&srv, sizeof(srv));
+            } else if (m == 0) {
+                // EOF — stop checking stdin
+                stdin_closed = 1;
+                pfds[0].events = 0;
+            } else if (errno != EAGAIN && errno != EWOULDBLOCK) {
+                perror("read");
+            }
+        }
+    }
+
+    close(sock);
     return 0;
 }
--- project/server.c
+++ project/server.c
@@ -1,41 +1,111 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <unistd.h>
 
+// helper to set a file descriptor to non-blocking mode
+static int set_nb(int fd) {
+    int f = fcntl(fd, F_GETFL, 0);
+    return f < 0 ? -1 : fcntl(fd, F_SETFL, f | O_NONBLOCK);
+}
+
 int main(int argc, char** argv) {
-    if (argc < 2) {
+    if (argc != 2) {
         fprintf(stderr, "Usage: server <port>\n");
-        exit(1);
+        return 1;
     }
 
     int port = atoi(argv[1]);
 
-    // TODO: Create socket
-
-    // TODO: Set stdin and socket nonblocking
-
-    // TODO: Construct server address
-    
-    // TODO: Bind address to socket
-    
-    // TODO: Create sockaddr_in and socklen_t buffers to store client address
-
-    char buffer[1024];
-    int client_connected = 0;
-
-    // Listen loop
-    while (1) {
-        // TODO: Receive from socket
-        // TODO: If no data and client not connected, continue
-        // TODO: If data, client is connected and write to stdout
-        // TODO: Read from stdin
-        // TODO: If data, send to socket
+    // TODO: create socket
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
     }
 
+    // TODO: construct server address
+    struct sockaddr_in loc = {0};
+    loc.sin_family = AF_INET;
+    loc.sin_addr.s_addr = htonl(INADDR_ANY);  // listen on all interfaces
+    loc.sin_port = htons(port);
+
+    // TODO: bind address to socket
+    if (bind(sock, (struct sockaddr*)&loc, sizeof(loc)) < 0) {
+        perror("bind");
+        return 1;
+    }
+
+    // TODO: set stdin and socket nonblocking
+    if (set_nb(STDIN_FILENO) < 0 || set_nb(sock) < 0) {
+        perror("fcntl");
+        return 1;
+    }
+
+    // we’ll use poll() to listen to both stdin and the socket
+    struct pollfd pfds[2] = {
+        { .fd = STDIN_FILENO, .events = 0 },     // will enable later after client sends first packet
+        { .fd = sock,         .events = POLLIN }
+    };
+
+    char buf[1024];  // required buffer size
+    int stdin_closed = 0;
+    int client_ready = 0;
+
+    // TODO: create sockaddr_in and socklen_t buffers to store client address
+    struct sockaddr_in client;
+    socklen_t clen = sizeof(client);
+
+    // listen loop
+    while (1) {
+        if (poll(pfds, 2, -1) < 0) {
+            perror("poll");
+            break;
+        }
+
+        // TODO: receive from socket
+        if (pfds[1].revents & POLLIN) {
+            ssize_t n = recvfrom(sock, buf, sizeof(buf), 0,
+                                 (struct sockaddr*)&client, &clen);
+
+            // TODO: if no data and client not connected, continue
+            // TODO: if data, client is connected and write to stdout
+            if (n > 0) {
+                if (!client_ready) {
+                    client_ready = 1;
+                    pfds[0].events = POLLIN;  // now we care about stdin
+                }
+                write(STDOUT_FILENO, buf, n);
+            } else if (n < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+                perror("recvfrom");
+            }
+        }
+
+        // TODO: read from stdin
+        // TODO: if data, send to socket
+        if (client_ready && !stdin_closed &&
+            (pfds[0].revents & POLLIN)) {
+
+            ssize_t m = read(STDIN_FILENO, buf, sizeof(buf));
+            if (m > 0) {
+                sendto(sock, buf, m, 0,
+                       (struct sockaddr*)&client, clen);
+            } else if (m == 0) {
+                // stdin closed (e.g. EOF), stop checking it
+                stdin_closed = 1;
+                pfds[0].events = 0;
+            } else if (errno != EAGAIN && errno != EWOULDBLOCK) {
+                perror("read");
+            }
+        }
+    }
+
+    close(sock);
     return 0;
 }
